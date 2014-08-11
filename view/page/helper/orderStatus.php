6e0516462e4a59f73b5b60644649c0cb1ba1ddc5
maskas <mangirdas@impresspages.org>
2014-08-11 13:05:05 +0300

    delivery price in order status

--- view/page/helper/orderStatus.php
+++ view/page/helper/orderStatus.php
@@ -12,6 +12,10 @@
         <td><?php echo esc(ipFormatPrice($order['price'], $order['currency'], 'PayPal')) ?></td>
     </tr>
     <tr>
+        <td><b><?php echo __('Delivery cost', 'PayPal') ?></b></td>
+        <td><?php echo esc(ipFormatPrice($order['deliveryCost'], $order['currency'], 'PayPal')) ?></td>
+    </tr>
+    <tr>
         <td><b><?php echo __('Date', 'PayPal') ?></b></td>
         <td><?php echo esc(ipFormatDateTime(strtotime($order['createdAt']), 'PayPal')) ?></td>
     </tr>
