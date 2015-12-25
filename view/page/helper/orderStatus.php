6511ef43fd93e6b61406962f1e5f2e0b7cac3961
Mangirdas Skripka <mangirdas@impresspages.org>
2015-12-25 19:36:19 +0200

    fix translations

--- view/page/helper/orderStatus.php
+++ view/page/helper/orderStatus.php
@@ -1,24 +1,24 @@
 <table>
     <tr>
-        <td><b><?php echo __('Order ID', 'PayPal') ?></b></td>
+        <td><b><?php echo __('Order ID', 'SimpleProduct') ?></b></td>
         <td><?php echo esc($order['id']) ?></td>
     </tr>
     <tr>
-        <td><b><?php echo __('Item', 'PayPal') ?></b></td>
+        <td><b><?php echo __('Item', 'SimpleProduct') ?></b></td>
         <td><?php echo esc($order['title']) ?></td>
     </tr>
     <tr>
-        <td><b><?php echo __('Price', 'PayPal') ?></b></td>
-        <td><?php echo esc(ipFormatPrice($order['price'], $order['currency'], 'PayPal')) ?></td>
+        <td><b><?php echo __('Price', 'SimpleProduct') ?></b></td>
+        <td><?php echo esc(ipFormatPrice($order['price'], $order['currency'], 'SimpleProduct')) ?></td>
     </tr>
 <?php if ($order['type'] == 'physical') { ?>
     <tr>
-        <td><b><?php echo __('Delivery cost', 'PayPal') ?></b></td>
-        <td><?php echo esc(ipFormatPrice($order['deliveryCost'], $order['currency'], 'PayPal')) ?></td>
+        <td><b><?php echo __('Delivery cost', 'SimpleProduct') ?></b></td>
+        <td><?php echo esc(ipFormatPrice($order['deliveryCost'], $order['currency'], 'SimpleProduct')) ?></td>
     </tr>
 <?php } ?>
     <tr>
-        <td><b><?php echo __('Date', 'PayPal') ?></b></td>
-        <td><?php echo esc(ipFormatDateTime(strtotime($order['createdAt']), 'PayPal')) ?></td>
+        <td><b><?php echo __('Date', 'SimpleProduct') ?></b></td>
+        <td><?php echo esc(ipFormatDateTime(strtotime($order['createdAt']), 'SimpleProduct')) ?></td>
     </tr>
 </table>
